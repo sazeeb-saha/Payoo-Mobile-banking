e25a479ba03641b3083aeff1266eb6180f437d9c
sazeeb-saha <sazeeb.dev@gmail.com>
2026-02-21 13:33:14 +0600

    show and hidden machine added to transfer money section

--- script/machine.js
+++ script/machine.js
@@ -20,9 +20,11 @@ function setBalance(value) {
 function showOnly(id) {
   const addMoney = document.getElementById("add-money");
   const cashOut = document.getElementById("cash-out");
+  const transferMoney = document.getElementById("transfer-money");
 
   addMoney.classList.add("hidden");
   cashOut.classList.add("hidden");
+  transferMoney.classList.add("hidden");
 
   const selected = document.getElementById(id);
   selected.classList.remove("hidden");
